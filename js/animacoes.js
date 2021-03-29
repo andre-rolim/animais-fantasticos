6a08497ee5083e354b37dcbd1847aac04a9c1787
Andre <andrel.rolimj@gmail.com>
2021-03-29 18:05:27 -0300

    ScrollSuave transformado em classe

--- js/animacoes.js
+++ js/animacoes.js
@@ -60,28 +60,6 @@ function initAccordion() {
 }
 initAccordion();
 
-// funcao para fazer o scroll suave no site
-function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]'
-  );
-
-  function scroollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scroollToSection);
-  });
-}
-initScrollSuave();
-
 // carregar conteudo ao fazer scroll no site
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import ScrollSuave from './modules/scroll-suave.js';
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdownmenu.js';
@@ -6,6 +7,9 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
 initModal();
 initTooltip();
